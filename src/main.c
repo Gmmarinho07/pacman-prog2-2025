4fe31e87f3b06feee3752b299abb0c8777dfa635
Gmmarinho07 <maiamarinho66@gmail.com>
2025-11-24 16:27:41 -0300

    .

--- src/main.c
+++ src/main.c
@@ -1,21 +1,3 @@
 #include "raylib.h"
 #include "game.h"
 
-int main(void) {
-
-    GameState game = {0};
-
-    if (!InitGame(&game, "maps/mapa1.txt")) {
-        return -1;
-    }
-
-    while (!WindowShouldClose()) {
-        float dt = GetFrameTime();
-        UpdateGame(&game, dt);
-        RenderGame(&game);
-    }
-
-    CleanupGame(&game);
-
-    return 0;
-}
